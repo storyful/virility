ba172913d98079f19511e33073c3d0b1027945ae
Gianni Leggio <giannileggio@gmail.com>
2019-05-10 16:22:25 +0200

    Facebook client can use an array of creds
    Co-authored-by: Liam Baker <liam.baker@storyful.com>

--- lib/virility/strategies/facebook.rb
+++ lib/virility/strategies/facebook.rb
@@ -1,11 +1,11 @@
 module Virility
   class Facebook < Strategy
     def base_url
-       'https://graph.facebook.com/' \
-                "?access_token=#{facebook_token}" \
-                '&fields=engagement' \
-                '&id='
-              end
+      'https://graph.facebook.com/' \
+              "?access_token=#{facebook_token}" \
+              '&fields=engagement' \
+              '&id='
+    end
 
     def census
       self.class.get("#{base_url}#{@url}",
@@ -14,8 +14,8 @@ module Virility
     end
 
     def facebook_token
-      oauth = Koala::Facebook::OAuth.new(ENV['FB_APP_ID'], ENV['FB_APP_SECRET'],
-         'https://virility.test')
+      oauth = Koala::Facebook::OAuth.new(fb_app_id, fb_app_secret,
+                                         'https://virility.test')
       token = Koala::Facebook::API.new(oauth.get_app_access_token)
       token.access_token
     end
@@ -32,12 +32,28 @@ module Virility
       results.dig('engagement_count') || 0
     end
 
-  private
+    private
 
     def valid_response_test
       @response.respond_to?(:parsed_response) && \
         @response.parsed_response.is_a?(Hash) && \
         !@response.parsed_response['engagement'].nil?
+    end
+
+    def fb_app_sample
+      @fb_app_sample ||= ENV['FB_APP_OAUTH_ARRAY']&.sample
+    end
+
+    def fb_app_id
+      return fb_app_sample[:id] if fb_app_sample
+
+      ENV['FB_APP_ID']
+    end
+
+    def fb_app_secret
+      return fb_app_sample[:secret] if fb_app_sample
+
+      ENV['FB_APP_SECRET']
     end
   end
 end
